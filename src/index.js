5284f6177a2568fb2f42cdde4a5c11a6585935e7
carlos505 <aragonaugusto24@gmail.com>
2023-08-28 11:59:43 -0600

    Refactor functions from promises to async/await

--- src/index.js
+++ src/index.js
@@ -6,41 +6,44 @@ const searchInput = document.getElementById('search-input')
 const resultsEl = document.querySelector('.results')
 const appName = document.querySelector('.app-name')
 
-;(function renderTrending () {
-    appName.addEventListener('click', () => location.reload())
+appName.addEventListener('click', () => location.reload())
 
-    fetch('https://api.giphy.com/v1/gifs/trending?api_key=Uw5xMl9pAcjoNUtsqc0SjYnPhUNRvVPI&limit=50')
-        .then(response => response.json())
-        .then(getGifs)
-        .catch(onError)
-})()
+async function renderTrending () {
+    const response = await fetch('https://api.giphy.com/v1/gifs/trending?api_key=Uw5xMl9pAcjoNUtsqc0SjYnPhUNRvVPI&limit=50',
+        { mode: 'cors' }
+    )
+    const data = await response.json()
+    getGifs(data)
+}
+renderTrending()
 
-function RenderResults (event) {
+async function renderResults (event) {
     event.preventDefault()
 
     const input = searchInput.value
-
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=Uw5xMl9pAcjoNUtsqc0SjYnPhUNRvVPI&q=${input}&limit=50`)
-        .then(response => response.json())
-        .then(response => {
-            gifsEl.innerHTML = ''
-            getGifs(response)
-            resultsEl.textContent = `Showing results for: "${input}"`
-        })
-        .catch(onError)
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=Uw5xMl9pAcjoNUtsqc0SjYnPhUNRvVPI&q=${input}&limit=50`,
+        { mode: 'cors' }
+    )
+    const data = await response.json()
+    clearContainer(input)
+    getGifs(data)
 
     searchContainer.reset()
 }
 
-searchContainer.addEventListener('submit', RenderResults)
+searchContainer.addEventListener('submit', renderResults)
 
-function getGifs (response) {
-    for (let i = 0; i < response.data.length; i++) {
+function getGifs (data) {
+    for (let i = 0; i < data.data.length; i++) {
         const img = document.createElement('img')
-        img.src = response.data[i].images.fixed_width.webp
-
+        img.src = data.data[i].images.fixed_width.webp
         gifsEl.appendChild(img)
     }
+}
+
+function clearContainer (input) {
+    gifsEl.innerHTML = ''
+    resultsEl.textContent = `Showing results for: "${input}"`
 }
 
 function onError (error) {
